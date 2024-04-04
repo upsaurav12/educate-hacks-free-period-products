e70bd2a8a6a4c1161ce712de191c0749bb1ac86e
Annabelle <lilacpaw2018@gmail.com>
2024-04-04 07:32:37 -0900

    all new locations added are automatically geocoded

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,10 @@
 class Location < ApplicationRecord
+    # validates :name, :street, :state_province, :country
+    geocoded_by :full_address
+    after_validation :geocode
+    def full_address
+        "#{street}, #{city}, #{state_province}, #{country}"
+      end
+
+   
 end
