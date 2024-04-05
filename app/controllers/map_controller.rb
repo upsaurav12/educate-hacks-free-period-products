d6f1b32bd1cd5e5e38c24cc46591ba2ab4e4d229
JasonJungler <jasons10219@gmail.com>
2024-04-05 20:22:56 +0800

    feat: added skeleton at `/inventories` endpoint

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,5 +1,11 @@
 class MapController < ApplicationController
+  @@mapbox_access_token = ENV['MAPBOX_ACCESS_TOKEN']
+ 
   def index
-    @mapbox_access_token = ENV['MAPBOX_ACCESS_TOKEN']
+    @mapbox_token = @@mapbox_access_token
+  end
+
+  def inventories
+    @mapbox_token = @@mapbox_access_token
   end
 end
